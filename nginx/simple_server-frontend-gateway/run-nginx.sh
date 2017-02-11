932930f2e2ca1ec6950f7547e6ace4bd1c4507e5
Pascal Kern <pascal.kern@gmail.com>
2017-02-11 14:43:26 +0100

    Updates some scripts and functions.

---
+++ nginx/simple_server-frontend-gateway/run-nginx.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# If not exist we need the dir bellow
+mkdir -p /etc/nginx/conf.d/sites-enabled
+
+# Link all sites which should be avaiable
+ln -sf /etc/nginx/conf.d/sites/test.conf /etc/nginx/conf.d/sites-enabled/test.conf
+ln -sf /etc/nginx/conf.d/sites/debug.conf /etc/nginx/conf.d/sites-enabled/debug.conf
+
+# Fire of nginx server
+nginx -g 'daemon off;'
--- nginx/simple_server-frontend-gateway/start-docker.sh
+++ nginx/simple_server-frontend-gateway/start-docker.sh
@@ -6,12 +6,36 @@
 
 
 docker build --rm -t nginx:pk .
+if [ $? -ne 0 ]; then
+    exit $?
+fi
 
-docker rm nginxPKc
+# Patch hosts file for experimental server name
+hostsFile=/etc/hosts
+if ! grep -q debug.imagic.ch "$hostsFile"; then
+    echo Tries to patch the hosts file: $hostsFile. Most likely we need root rights.
+    echo "
+# Host for docker experiment
+172.0.0.1   debug.imagic.ch
+" >> "$hostsFile" 2>/dev/null
+    if [ $? -ne 0 ]; then
+        echo COULD NOT PATCH THE HOSTSFILE! PLEASE RERUN THE START SCRIPT AS ROOT!
+    fi
+fi
 
+
+# Remove container with this name if still exists. Else can not run the image with the same container name!
+docker rm nginxPKc 2>/dev/null
+
+
+# More or less just a configuartion documentation.
+# Config which is usually also possible to set with docker-compose.yml!
 docker run --name nginxPKc \
     -v `pwd`/config/usrShareNginx:/usr/share/nginx \
     -v `pwd`/config/conf.d:/etc/nginx/conf.d \
     -v `pwd`/html:/usr/local/share/nginx/html \
     -p 80:80 \
     nginx:pk
+
+
+
