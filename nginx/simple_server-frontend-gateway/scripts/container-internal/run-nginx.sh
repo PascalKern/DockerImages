9b1052e8f0253e93042c09e180591a7ff76f7f05
Pascal Kern <pascal.kern@gmail.com>
2017-02-13 20:42:59 +0100

    Tries to get subdomain redirecting working. Unfortunatelly this DOES NOT WORK on docker for mac (edit of hosts file does not work!).

--- nginx/simple_server-frontend-gateway/scripts/container-internal/run-nginx.sh
+++ nginx/simple_server-frontend-gateway/scripts/container-internal/run-nginx.sh
@@ -15,6 +15,7 @@ mkdir -p /etc/nginx/conf.d/sites-enabled
 
 # Link all sites which should be avaiable
 ln -sf /etc/nginx/conf.d/sites/test.conf /etc/nginx/conf.d/sites-enabled/test.conf
+echo "127.0.0.1     debug.imagic.ch" >> /etc/hosts
 # ln -sf /etc/nginx/conf.d/sites/debug.conf /etc/nginx/conf.d/sites-enabled/debug.conf
 
 if [[  $MODE -eq Developement ]]; then
